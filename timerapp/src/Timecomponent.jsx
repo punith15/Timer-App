5b72574a6a0365ae4f22c225e2ab5334f9f16011
punith <punith@gmail.com>
2020-05-22 10:43:48 +0530

    Timer-App:v1

--- timerapp/src/Timecomponent.jsx
+++ timerapp/src/Timecomponent.jsx
@@ -9,7 +9,7 @@ export const Timercomponent = (props)=>{
         <div id="main" style={{backgroundImage:`url(${props.day})`}} className="container-fluid">
         <div id="mainText" className="col-md-6">
         <h1>Hello, <span style={timecolor}>Good {props.wish} !!!</span></h1>
-        <div>
+        <div id="timebox">
           <button>{props.timeSlice[0]}</button>
           <button>{props.timeSlice[1]}</button>
           <button>{props.timeSlice[2]}</button>
